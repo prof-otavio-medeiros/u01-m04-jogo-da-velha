3b803133a1527035fe233dc3834837f6e87ef55f
Prof. Otávio Medeiros <prof.otavio.medeiros@gmail.com>
2022-09-22 23:03:59 -0300

    commit 2022 09 22 001

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,30 @@
-let jogadorAtual = document.getElementById("jogadorInicial").value;
-let started = false;
-
-document.getElementById("jogador").innerText = "Jogador: " + jogadorAtual;
-document.getElementById("message").innerText = "Jogo Não Iniciado";
-
-function setCurrentPlayer(jogador) {
-  document.getElementById("jogador").innerText = "Jogador: " + jogador;
+function getFirstPlayer() {
+  if (document.getElementById("jogadorInicial1").checked) {
+    return "X";
+  } else {
+    return "O";
+  }
 }
 
-function setFirstPlayer() {
-  jogadorAtual = document.getElementById("jogadorInicial").value;
-  setCurrentPlayer(jogadorAtual);
+let jogadorAtual = getFirstPlayer();
+let started = false;
+document.getElementById("jogador").innerHTML =
+  "Próximo Jogador<br>" + getCurretPlayer(jogadorAtual);
+document.getElementById("message").innerText = "Jogo Não Iniciado";
+
+function getCurretPlayer(jogador) {
+  return jogador === "X"
+    ? "<img src='imgs/Ironman.png'>"
+    : "<img src='imgs/Spiderwoman.png'>";
+}
+
+function setCurrentPlayer(jogador) {
+  document.getElementById("jogador").innerHTML =
+    "Próximo Jogador<br>" + getCurretPlayer(jogador);
 }
 
 function restart() {
-  jogadorAtual = document.getElementById("jogadorInicial").value;
-  started = true;
+  jogadorAtual = getFirstPlayer();
   for (let element of document.getElementsByClassName("grid-container")[0]
     .children) {
     element.style.backgroundColor = "#faebd7";
@@ -28,15 +37,19 @@ function restart() {
 
 function init(element) {
   restart();
+  started = true;
+  for (let element of document.getElementsByName("jogadorInicial")) {
+    element.disabled = true;
+  }
   mark(element);
 }
 
 function checkWinSequence(jogadas, posicao01, posicao02, posicao03) {
   if (
-    jogadas[posicao01].innerText === jogadas[posicao02].innerText &&
-    jogadas[posicao02].innerText === jogadas[posicao03].innerText &&
-    jogadas[posicao03].innerText &&
-    jogadas[posicao03].innerText.length !== 0
+    jogadas[posicao01].innerHTML === jogadas[posicao02].innerHTML &&
+    jogadas[posicao02].innerHTML === jogadas[posicao03].innerHTML &&
+    jogadas[posicao03].innerHTML &&
+    jogadas[posicao03].innerHTML.length !== 0
   ) {
     jogadas[posicao01].style.backgroundColor = "yellow";
     jogadas[posicao02].style.backgroundColor = "yellow";
@@ -69,27 +82,37 @@ function checkNoWin() {
   let noWin = true;
   for (let element of document.getElementsByClassName("grid-container")[0]
     .children) {
-    noWin = noWin && element.innerText !== "";
+    noWin = noWin && element.innerHTML !== "";
   }
-  if(noWin) {
+  if (noWin) {
     for (let element of document.getElementsByClassName("grid-container")[0]
-    .children) {
+      .children) {
       element.style.backgroundColor = "aqua";
-  }
+    }
+    for (let element of document.getElementsByName("jogadorInicial")) {
+      element.disabled = false;
+    }
   }
   return noWin;
 }
 
 function mark(element) {
   let gameOver = true;
-  if (element.innerText.length === 0 && started) {
-    element.innerText = jogadorAtual;
+  if (element.innerHTML.length === 0 && started) {
+    element.innerHTML =
+      jogadorAtual === "X"
+        ? "<img src='imgs/Ironman.png'>"
+        : "<img src='imgs/Spiderwoman.png'>";
     jogadorAtual = jogadorAtual === "X" ? "O" : "X";
     setCurrentPlayer(jogadorAtual);
     document.getElementById("message").innerText = "Jogo em Andamento";
     if (checkWin()) {
-      document.getElementById("message").innerText =
-        "O Jogador '" + element.innerText + "' Venceu!";
+      document.getElementById("jogador").innerHTML =
+        "VENCEDOR<br>" + element.innerHTML;
+      document.getElementById("message").innerText = "Reinicie o Jogo";
+      for (let element of document.getElementsByName("jogadorInicial")) {
+        element.disabled = false;
+      }
     } else if (checkNoWin()) {
       document.getElementById("message").innerText = "Jogo Sem Ganhadores!";
     } else {
@@ -99,12 +122,8 @@ function mark(element) {
       document.getElementById("restart").type = "button";
       started = false;
     }
-  } else if (!started) {
-    if (document.getElementById("restart").type === "hidden") {
-      init(element);
-    } else {
-      document.getElementById("message").innerText = "Reinicie o Jogo";
-    }
+  } else if (!started && document.getElementById("restart").type === "hidden") {
+    init(element);
   } else if (document.getElementById("restart").type === "hidden") {
     document.getElementById("message").innerText = "Posição em Uso!";
   }
